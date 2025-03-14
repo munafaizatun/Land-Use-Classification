f235d02bb9b3f6e6af300ef858f9659553e27a24
Muna Faizatun Nabilah <munafaizatunnabilahh@gmail.com>
2025-03-14 16:15:51 +0700

    create descriptive text.R

--- scripts/descriptive text.R
+++ scripts/descriptive text.R
@@ -1,5 +1,5 @@
-rm(list=ls()) # Menghapus semua dataset dan variabel
-graphics.off() # Menutup semua grafik
+rm(list=ls()) #Deleting all datasets and variables
+graphics.off() #Close all charts
 
 library(ggplot2)
 a<-read.csv("2022 train.csv")
@@ -12,7 +12,7 @@ names(b)[names(b) == "Iterasi"] <- "Repetition"
 names(c)[names(c) == "Iterasi"] <- "Repetition"
 names(d)[names(d) == "Iterasi"] <- "Repetition"
 
-##buffering
+#buffering
 #ggplot(a, aes(x = Repetition, y = Accuracy, color = Method))+
   #geom_line()+ 
   #scale_color_manual(values = c("red", "blue", "green"))+
@@ -20,26 +20,27 @@ names(d)[names(d) == "Iterasi"] <- "Repetition"
        #x = "Repetition",
        #y = "Accuracy")
 
-#atau
+#OR
 #ggplot(data, aes(x = Jarak, y = Persentase, group = Obyek, color = Obyek))+
   #geom_line() + 
   #scale_color_manual(values = c("red", "blue", "green")) +
   #labs(x = "Jarak (Km)",
        #y = "Persentase")
+
 dev.list()
-##TRAIN 2022
+#TRAIN 2022
 aa = ggplot(a, aes(x=Repetition, y=Accuracy, group=Method, color=Method)) +
   geom_line(linewidth = 1) +
   labs(x = "Repetition", y = "Accuracy") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran label sumbu x
-    axis.title.y = element_text(size = 15),  # Ukuran label sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y
+    axis.title.x = element_text(size = 15),  #X-axis label size
+    axis.title.y = element_text(size = 15),  #Y-axis label size
+    axis.text.x = element_text(size = 12),   #Size of numbers on the x-axis
+    axis.text.y = element_text(size = 12)    #Size of numbers on the y-axis #Y-axis text size
   )
 
-png("/cloud/project/Akurasi 2022 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("Akurasi 2022 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(aa)
 dev.off()
 
@@ -48,13 +49,13 @@ bb = ggplot(a, aes(x=Repetition, y=Sensitivity, group=Method, color=Method))+
   labs(x = "Repetition", y = "Sensitivity") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15),   
+    axis.text.x = element_text(size = 12),   
+    axis.text.y = element_text(size = 12)    
     );
 
-png("/cloud/project/Sensitivitas 2022 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("Sensitivitas 2022 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(bb)
 dev.off()
 
@@ -63,13 +64,13 @@ cc = ggplot(a, aes(x=Repetition, y=Specificity, group=Method, color=Method))+
   labs(x = "Repetition", y = "Specificity") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15),  
+    axis.text.x = element_text(size = 12),  
+    axis.text.y = element_text(size = 12)   
   );
 
-png("/cloud/project/Spesifisitas 2022 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("Spesifisitas 2022 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(cc)
 dev.off()
 
@@ -78,29 +79,29 @@ dd = ggplot(a, aes(x=Repetition, y=AUC, group=Method, color=Method))+
   labs(x = "Repetition", y = "AUC") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15),  
+    axis.text.x = element_text(size = 12),   
+    axis.text.y = element_text(size = 12)   
   );
 
-png("/cloud/project/AUC 2022 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("AUC 2022 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(dd)
 dev.off()
 
-##TEST 2022
+#TEST 2022
 ee = ggplot(b, aes(x=Repetition, y=Accuracy, group=Method, color=Method))+
   geom_line(linewidth = 1) +
   labs(x = "Repetition", y = "Accuracy") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y   # Ukuran teks sumbu y
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15), 
+    axis.text.x = element_text(size = 12),  
+    axis.text.y = element_text(size = 12)    
   );
 
-png("/cloud/project/Akurasi 2022 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("Akurasi 2022 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(ee)
 dev.off()
 
@@ -109,13 +110,13 @@ ff = ggplot(b, aes(x=Repetition, y=Sensitivity, group=Method, color=Method))+
   labs(x = "Repetition", y = "Sensitivity") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y   # Ukuran teks sumbu y
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15),   
+    axis.text.x = element_text(size = 12),  
+    axis.text.y = element_text(size = 12)  
   );
 
-png("/cloud/project/Sensitivitas 2022 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("Sensitivitas 2022 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(ff)
 dev.off()
 
@@ -124,13 +125,13 @@ gg = ggplot(b, aes(x=Repetition, y=Specificity, group=Method, color=Method))+
   labs(x = "Repetition", y = "Specificity") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y   # Ukuran teks sumbu y
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15),   
+    axis.text.x = element_text(size = 12),   
+    axis.text.y = element_text(size = 12)    
   );
 
-png("/cloud/project/Spesifisitas 2022 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("Spesifisitas 2022 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(gg)
 dev.off()
 
@@ -139,13 +140,13 @@ hh = ggplot(b, aes(x=Repetition, y=AUC, group=Method, color=Method))+
   labs(x = "Repetition", y = "AUC") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y   # Ukuran teks sumbu y
+    axis.title.x = element_text(size = 15), 
+    axis.title.y = element_text(size = 15),   
+    axis.text.x = element_text(size = 12),  
+    axis.text.y = element_text(size = 12)   
   );
 
-png("/cloud/project/AUC 2022 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("AUC 2022 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(hh)
 dev.off()
 
@@ -160,13 +161,13 @@ ii = ggplot(c, aes(x=Repetition, y=Accuracy, group=Method, color=Method))+
   labs(x = "Repetition", y = "Accuracy") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y   # Ukuran teks sumbu y
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15),  
+    axis.text.x = element_text(size = 12),  
+    axis.text.y = element_text(size = 12) 
   );
 
-png("/cloud/project/Akurasi 2023 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("Akurasi 2023 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(ii)
 dev.off()
 
@@ -175,13 +176,13 @@ jj = ggplot(c, aes(x=Repetition, y=Sensitivity, group=Method, color=Method))+
   labs(x = "Repetition", y = "Sensitivity") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y   # Ukuran teks sumbu y
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15),   
+    axis.text.x = element_text(size = 12),  
+    axis.text.y = element_text(size = 12)   
   );
 
-png("/cloud/project/Sensitivitas 2023 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("Sensitivitas 2023 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(jj)
 dev.off()
 
@@ -190,13 +191,13 @@ kk = ggplot(c, aes(x=Repetition, y=Specificity, group=Method, color=Method))+
   labs(x = "Repetition", y = "Specificity") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y   # Ukuran teks sumbu y
+    axis.title.x = element_text(size = 15), 
+    axis.title.y = element_text(size = 15),   
+    axis.text.x = element_text(size = 12), 
+    axis.text.y = element_text(size = 12)    
   );
 
-png("/cloud/project/Spesifisitas 2023 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("Spesifisitas 2023 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(kk)
 dev.off()
 
@@ -205,13 +206,13 @@ ll = ggplot(c, aes(x=Repetition, y=AUC, group=Method, color=Method))+
   labs(x = "Repetition", y = "AUC") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y   # Ukuran teks sumbu y
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15),   
+    axis.text.x = element_text(size = 12),  
+    axis.text.y = element_text(size = 12)   
   );
 
-png("/cloud/project/AUC 2023 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("AUC 2023 train.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(ll)
 dev.off()
 
@@ -221,13 +222,13 @@ mm = ggplot(d, aes(x=Repetition, y=Accuracy, group=Method, color=Method))+
   labs(x = "Repetition", y = "Accuracy") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y   # Ukuran teks sumbu y
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15),  
+    axis.text.x = element_text(size = 12),  
+    axis.text.y = element_text(size = 12)    
   );
 
-png("/cloud/project/Akurasi 2023 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("Akurasi 2023 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(mm)
 dev.off()
 
@@ -236,13 +237,13 @@ nn = ggplot(d, aes(x=Repetition, y=Sensitivity, group=Method, color=Method))+
   labs(x = "Repetition", y = "Sensitivity") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y   # Ukuran teks sumbu y
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15),   
+    axis.text.x = element_text(size = 12),   
+    axis.text.y = element_text(size = 12)   ]
   );
 
-png("/cloud/project/Sensitivitas 2023 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("Sensitivitas 2023 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(nn)
 dev.off()
 
@@ -251,13 +252,13 @@ oo = ggplot(d, aes(x=Repetition, y=Specificity, group=Method, color=Method))+
   labs(x = "Repetition", y = "Specificity") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y   # Ukuran teks sumbu y
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15),   
+    axis.text.x = element_text(size = 12),   
+    axis.text.y = element_text(size = 12)    
   );
 
-png("/cloud/project/Spesifisitas 2023 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("Spesifisitas 2023 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(oo)
 dev.off()
 
@@ -266,47 +267,20 @@ pp = ggplot(d, aes(x=Repetition, y=AUC, group=Method, color=Method))+
   labs(x = "Repetition", y = "AUC") + 
   theme(
     legend.position = "none",
-    axis.title.x = element_text(size = 15),  # Ukuran teks sumbu x
-    axis.title.y = element_text(size = 15),   # Ukuran teks sumbu y
-    axis.text.x = element_text(size = 12),   # Ukuran angka pada sumbu x
-    axis.text.y = element_text(size = 12)    # Ukuran angka pada sumbu y   # Ukuran teks sumbu y
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15),  
+    axis.text.x = element_text(size = 12),   
+    axis.text.y = element_text(size = 12)    
   );
 
-png("/cloud/project/AUC 2023 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
+png("AUC 2023 test.png", width = 6 * 300, height = 4 * 300, res = 300)  # 6x4 inci @ 300 DPI
 print(pp)
 dev.off()
 
 thn23 = ggarrange(ii, jj, kk, ll, mm, nn, oo, pp,
                   ncol = 3, nrow = 3);thn23
 
-dev.size("px")  # Cek ukuran dalam piksel
-dev.size("in")  # Cek ukuran dalam inci
+dev.size("px")  #Check the size in pixels
+dev.size("in")  #Check the size in inches
 
-# Membuka device PNG untuk menyimpan plot
-png("/cloud/project/method.png", width = 6 * 300, height = 4 * 300, res = 300)
-
-# Membuat plot dengan hanya legenda tanpa menampilkan grafik
-aaa = ggplot(a, aes(x = Repetition, y = Accuracy, group = Method, color = Method)) +
-  geom_line(linewidth = 1) +
-  labs(x = "Repetition", y = "Accuracy", color = "Classification Method") + 
-  guides(
-    color = guide_legend(title = "Classification Method"),  # Menampilkan legenda untuk `Method`
-    linetype = "none",  # Menyembunyikan legenda untuk `linetype`
-    size = "none"  # Menyembunyikan legenda untuk `size`
-  ) +
-  theme(
-    legend.position = "right", 
-    legend.title = element_text(size = 14, face = "bold"),
-    legend.text = element_text(size = 12),
-    axis.title.x = element_text(size = 15),
-    axis.title.y = element_text(size = 15),
-    axis.text.x = element_text(size = 12),
-    axis.text.y = element_text(size = 12)
-  )
-
-# Menampilkan plot ke file (legend + tanpa grafik)
-print(aaa)
-
-# Menutup device
-dev.off()
 
